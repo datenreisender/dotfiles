f7b2a94251d5db38c7adf58b84071df7206e7b6d
Marko Schulz <Marko.Schulz@gmail.com>
2018-02-09 08:39:59 +0100

    Add more git abbreviations

--- .config/fish/conf.d/abbr.fish
+++ .config/fish/conf.d/abbr.fish
@@ -4,11 +4,13 @@ abbr -a ga git add
 abbr -a gc git commit
 abbr -a gca git commit --all
 abbr -a gcnv git commit --no-verify
+abbr -a gcanv git commit --all --no-verify
 abbr -a gco git checkout
 abbr -a gd git diff
 abbr -a gdc git diff --cached
 abbr -a gl git log
 abbr -a gp git push
+abbr -a gpf git push --force
 abbr -a gs git status
 abbr -a gsp git smart-pull
 abbr -a gst git status
