469d6c0c47f812426431c583de75955bc37c47a9
Marko Schulz <Marko.Schulz@nordicsemi.no>
2019-12-20 14:56:04 +0100

    Add git abbreviations

--- .config/fish/conf.d/abbr.fish
+++ .config/fish/conf.d/abbr.fish
@@ -1,11 +1,11 @@
 abbr g git
-abbr gaa git add --all
 abbr ga git add
+abbr gaa git add --all
 abbr gc git commit
 abbr gca git commit --all
+abbr gcanv git commit --all --no-verify
 abbr gcf 'git add --all && git commit --no-verify & npm t && npm run lint'
 abbr gcnv git commit --no-verify
-abbr gcanv git commit --all --no-verify
 abbr gco git checkout
 abbr gd git diff
 abbr gdc git diff --cached
@@ -13,9 +13,11 @@ abbr gl git log
 abbr gp git pull
 abbr gpf git push --force-with-lease
 abbr gps git push
-abbr gs git status
+abbr gr git restore
+abbr gs git show
 abbr gsp git smart-pull
 abbr gst git status
+abbr gsw git switch
 
 abbr cask brew cask
 
