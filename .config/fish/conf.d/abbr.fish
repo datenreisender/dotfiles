9dbf2dbb553e2c85f461e367572c563688d84422
Marko Schulz <Marko.Schulz@nordicsemi.no>
2023-01-23 12:32:18 +0100

    Update some configs

--- .config/fish/conf.d/abbr.fish
+++ .config/fish/conf.d/abbr.fish
@@ -3,21 +3,24 @@ abbr ga git add
 abbr gaa git add --all
 abbr gc git commit
 abbr gca git commit --all
-abbr gcanv git commit --all --no-verify
-abbr gcf 'git add --all && git commit --no-verify & npm t && npm run lint'
+abbr gcan git commit --amend --no-edit
+abbr gcf git commit --fixup
 abbr gcnv git commit --no-verify
 abbr gco git checkout
 abbr gd git diff
 abbr gdc git diff --cached
 abbr gl git log
+abbr gll git lol
 abbr gp git pull
 abbr gpf git push --force-with-lease
 abbr gps git push
 abbr gr git restore
+abbr gra git restore -S -W .
 abbr gs git show
 abbr gsp git smart-pull
 abbr gst git status
 abbr gsw git switch
+abbr gsc git switch -c
 
 abbr cask brew cask
 
--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,1 +1,12 @@
-set -g fish_user_paths "/usr/local/opt/ruby/bin" $fish_user_paths
+set -g fish_user_paths /usr/local/opt/ruby/bin $fish_user_paths
+
+#set NODE_TO_USE 14
+#set -g fish_user_paths "/usr/local/opt/node@$NODE_TO_USE/bin" $fish_user_paths
+#set -gx LDFLAGS "-L/usr/local/opt/node@$NODE_TO_USE/lib"
+#set -gx CPPFLAGS "-I/usr/local/opt/node@$NODE_TO_USE/include"
+
+# set -g LANG en_GB.UTF-8
+
+# Enable VS code terminal shell integration manually: https://code.visualstudio.com/docs/terminal/shell-integration#_manual-installation
+string match -q "$TERM_PROGRAM" vscode
+and . (code --locate-shell-integration-path fish)
