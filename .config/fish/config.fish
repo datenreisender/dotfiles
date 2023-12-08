38ebfb271ff53e53c6b8e41339a6d908bd42ae54
Marko Schulz <Marko.Schulz@nordicsemi.no>
2023-12-08 13:19:04 +0100

    Add brew paths

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,10 @@
+set -gx HOMEBREW_PREFIX "/opt/homebrew";
+set -gx HOMEBREW_CELLAR "/opt/homebrew/Cellar";
+set -gx HOMEBREW_REPOSITORY "/opt/homebrew";
+! set -q PATH; and set PATH ''; set -gx PATH "/opt/homebrew/bin" "/opt/homebrew/sbin" $PATH;
+! set -q MANPATH; and set MANPATH ''; set -gx MANPATH "/opt/homebrew/share/man" $MANPATH;
+! set -q INFOPATH; and set INFOPATH ''; set -gx INFOPATH "/opt/homebrew/share/info" $INFOPATH;
+
 set -g fish_user_paths /usr/local/opt/ruby/bin $fish_user_paths
 
 # set NODE_TO_USE 18
